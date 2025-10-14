b52f4c4dae2f26ab0f1a2d5f4e85c3dc1c43abfc
Marcus Boden <marcus.boden@canonical.com>
2025-10-14 16:06:30 +0200

    complete rework of the whole charm

--- src/charm.py
+++ src/charm.py
@@ -14,12 +14,15 @@ https://juju.is/docs/sdk/create-a-minimal-kubernetes-charm
 
 import logging
 
+import ops.model
 from ops.charm import CharmBase, UpdateStatusEvent
-from ops.framework import StoredState
 from ops.main import main
 from ops.model import ActiveStatus, ErrorStatus
 
 import nginx
+from nginx import RevProxy
+import yaml
+from pydantic import ValidationError
 
 # Log messages can be retrieved using juju debug-log
 logger = logging.getLogger(__name__)
@@ -35,34 +38,86 @@ class ReverseProxyCharm(CharmBase):
         self.framework.observe(self.on.start, self._on_start)
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.upgrade_charm, self._on_upgrade_charm)
 
     def _on_start(self, _):
         """Handle start event."""
         self.unit.status = ActiveStatus()
 
     def _on_install(self, _):
+        self.unit.status = ops.model.MaintenanceStatus("Installing nginx")
         nginx.install()
         nginx.disable_default()
 
+        self.unit.status = ops.model.ActiveStatus()
+
+    def _on_upgrade_charm(self, _):
+        nginx.remove_old_files_proxies()
+
     def _on_config_changed(self, _):
+        self.unit.status = ops.model.MaintenanceStatus("Configuring Proxies")
+        existing_proxies = RevProxy.from_filesystem()
+        new_proxies = list()
 
-        http_reverse_proxies = self.config.get("http-reverse-proxies", "")
+        if reverse_proxies_yaml := self.config.get("reverse-proxies", False):
+            try:
+                reverse_proxies = yaml.safe_load(reverse_proxies_yaml)
+            except yaml.YAMLError as exc:
+                logger.error(exc)
+                self.unit.status = ops.model.BlockedStatus("Cannot load yaml")
+                return
 
-        http_reverse_proxies = http_reverse_proxies.split(",")
+            for proxy_conf in reverse_proxies:
+                try:
+                    proxy = RevProxy(**proxy_conf)
+                except ValidationError as exc:
+                    logger.warning(f"Config: {proxy_conf}\nError:{exc.errors()}")
+                    self.unit.status = ops.BlockedStatus("Cannot validate config")
+                    return
 
-        http_reverse_proxies = get_proxies(http_reverse_proxies)
+                if proxy not in existing_proxies:
+                    proxy.configure()
 
-        if len(http_reverse_proxies) > 0:
-            nginx.configure_reverse_proxies(http_reverse_proxies)
+                new_proxies.append(proxy)
 
-        https_reverse_proxies = self.config.get("https-reverse-proxies", "")
+        else:
+            if not (
+                self.config.get("http-reverse-proxy", False)
+                or self.config.get("http-reverse-proxy", False)
+            ):
+                self.unit.status = ops.model.BlockedStatus("No proxies configured")
+            else:
+                logger.warning("http[s]-reverse-proxies is deprecated.")
+            http_reverse_proxies = self.config.get("http-reverse-proxies", "")
+            http_reverse_proxies = http_reverse_proxies.split(",")
+            http_reverse_proxies = get_proxies(http_reverse_proxies)
 
-        https_reverse_proxies = https_reverse_proxies.split(",")
+            for proxy_dict in http_reverse_proxies:
+                name = f"http-auto-{proxy_dict['host-port']}-{proxy_dict['remote-ip']}-{proxy_dict['remote-port']}"
+                d = proxy_dict | {"name": name, "https": False}
+                proxy = RevProxy(**d)
+                if proxy not in existing_proxies:
+                    proxy.configure()
+                new_proxies.append(proxy)
 
-        https_reverse_proxies = get_proxies(https_reverse_proxies)
+            https_reverse_proxies = self.config.get("https-reverse-proxies", "")
+            https_reverse_proxies = https_reverse_proxies.split(",")
+            https_reverse_proxies = get_proxies(https_reverse_proxies)
 
-        if len(https_reverse_proxies) > 0:
-            nginx.configure_reverse_proxies(https_reverse_proxies, https=True)
+            for proxy_dict in https_reverse_proxies:
+                name = f"https-auto-{proxy_dict['host-port']}-{proxy_dict['remote-ip']}-{proxy_dict['remote-port']}"
+                d = proxy_dict | {"name": name, "https": True}
+                proxy = RevProxy(**d)
+                if proxy not in existing_proxies:
+                    proxy.configure()
+                proxy.configure()
+                new_proxies.append(proxy)
+
+        for proxy in existing_proxies:
+            if proxy not in new_proxies:
+                proxy.remove()
+
+        self.unit.status = ops.model.ActiveStatus()
 
 
 def get_proxies(proxies):
--- src/nginx.py
+++ src/nginx.py
@@ -3,8 +3,17 @@ import subprocess
 from pathlib import Path
 
 from charms.operator_libs_linux.v0 import apt
+from charms.operator_libs_linux.v1.systemd import service_reload, SystemdError
 from jinja2 import Environment, FileSystemLoader
+from pydantic import BaseModel, PositiveInt, Field, AliasChoices
+from pydantic.networks import IPvAnyAddress
+from typing import Optional
 
+import json
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 SVC = PKG = "nginx"
 
@@ -12,8 +21,74 @@ SITE_AVAILABLE = Path("/etc/nginx/sites-available")
 SITE_ENABLED = Path("/etc/nginx/sites-enabled")
 DEFAULT_SITE = "default"
 ENV = Environment(loader=FileSystemLoader("src/"))
+TEMPLATE = ENV.get_template("rev-proxy-template.conf")
 REVERSE_HTTP_PROXY_CONF = "juju-http-reverse-proxy.conf"
 REVERSE_HTTPS_PROXY_CONF = "juju-https-reverse-proxy.conf"
+PREFIX = "juju"
+
+
+class RevProxy(BaseModel):
+    """Class for a single Proxy."""
+
+    name: str
+    https: bool
+    # this should be doable with a generator, but I coudln't quite get it to work.
+    host_port: PositiveInt = Field(validation_alias=AliasChoices("host-port", "host_port"))
+    remote_ip: IPvAnyAddress = Field(validation_alias=AliasChoices("remote-ip", "remote_ip"))
+    remote_port: PositiveInt = Field(validation_alias=AliasChoices("remote-port", "remote_port"))
+    link: Optional[Path] = None
+    file: Optional[Path] = None
+
+    def model_post_init(self, context) -> None:
+        if self.host_port > 65535:
+            raise ValueError("RevProxy host port must be less than 65535")
+        self.link = SITE_ENABLED / f"{PREFIX}-{self.name}.conf"
+        self.file = SITE_AVAILABLE / f"{PREFIX}-{self.name}.conf"
+
+    @classmethod
+    def from_filesystem(cls) -> list:
+        proxies = list()
+        logger.debug("Reading existing proxies from file-system")
+        for p in SITE_ENABLED.glob(f"{PREFIX}-*.conf"):
+            logger.debug(f"found {p}")
+            with p.open() as f:
+                for line in f:
+                    if line.startswith("# pydantic-model:"):
+                        model_str = line.split(":", maxsplit=1)[1]
+                        logger.debug(f"proxy definition found: {model_str}. Trying to parse")
+                        proxies.append(RevProxy(**json.loads(model_str)))
+                        logger.debug(f"parsed {proxies[-1]}")
+        return proxies
+
+    def configure(self):
+        logger.info(f"Configuring {self.link}")
+        self.disable()
+        proxy_conf = TEMPLATE.render(
+            proxy=self.dict() | {"model": self.json(exclude={"file", "link"})}
+        )
+        with open(self.file, "w") as proxy_file:
+            proxy_file.write(proxy_conf)
+
+        self.enable()
+
+    def remove(self):
+        self.disable()
+        logger.info(f"Removing {self.file}")
+        self.file.unlink()
+
+    def enable(self):
+        logger.debug(f"Enabling {self.link}")
+        try:
+            self.link.symlink_to(target=self.file, target_is_directory=False)
+        except NotImplemented as e:
+            logger.error(e)
+            raise e
+        reload()
+
+    def disable(self):
+        logger.debug(f"Disabling {self.link}")
+        self.link.unlink(missing_ok=True)
+        reload()
 
 
 def install():
@@ -31,55 +106,23 @@ def install():
 
 
 def reload():
-    cmd = ["systemctl", "reload", SVC]
     try:
-        subprocess.check_output(cmd)
-    except subprocess.CalledProcessError:
-        raise
-
-
-def disable_site(site_name):
-    cmd = ["rm", SITE_ENABLED / site_name]
-
-    try:
-        if os.path.exists(SITE_ENABLED / site_name):
-            subprocess.check_output(cmd, stderr=None)
-    except subprocess.CalledProcessError:
-        pass
-
-    reload()
-
-
-def enable_site(site_name):
-    cmd = ["ln", "-s", SITE_AVAILABLE / site_name, SITE_ENABLED / site_name]
-    try:
-        subprocess.check_call(cmd)
-    except subprocess.CalledProcessError:
-        raise
-
-    reload()
+        service_reload(SVC)
+    except SystemdError as e:
+        raise e
 
 
 def disable_default():
-    disable_site(DEFAULT_SITE)
+    f = SITE_ENABLED / DEFAULT_SITE
+    f.unlink(missing_ok=True)
+    reload()
 
 
-def configure_reverse_proxies(reverse_proxies, https=False):
-    site = REVERSE_HTTP_PROXY_CONF
-    if https:
-        site = REVERSE_HTTPS_PROXY_CONF
+def remove_old_files_proxies():
+    for f in [REVERSE_HTTP_PROXY_CONF, REVERSE_HTTPS_PROXY_CONF]:
+        av = SITE_ENABLED / f
+        av.unlink(missing_ok=True)
+        en = SITE_AVAILABLE / f
+        en.unlink(missing_ok=True)
 
-    disable_site(site)
-
-    proxy_conf = ENV.get_template(site).render(reverse_proxies=reverse_proxies)
-    with open(SITE_AVAILABLE / site, "w") as proxy_file:
-        proxy_file.write(proxy_conf)
-
-    enable_site(site)
-
-
-def remove_reverse_proxies(https=False):
-    site = REVERSE_HTTP_PROXY_CONF
-    if https:
-        site = REVERSE_HTTPS_PROXY_CONF
-    disable_site(site)
+    reload()
